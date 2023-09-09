97192bc8bce4a7ffe1483764f10fb34a7ae57644
Kent Martin Åsvang <kent@asvang.no>
2023-09-09 14:51:23 +0200

    started on deploy-script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# script to move files where they need to be to work on production server
+
+# get input from user
+echo "Enter the name of the website you want to deploy:"
+
+# read input from user
+read WEBSITE
+
+
+
