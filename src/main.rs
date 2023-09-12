1735ba3d5f987883fedcd4f835848f3b04b6d369
Kent Martin Åsvang <github@asvang.no>
2023-09-12 14:01:00 +0200

    we are serving html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket_dyn_templates::{Template, context};
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! { title: "miniblog" })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
